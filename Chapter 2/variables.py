9b36bd415b5d0494e6bfe6fe16218a15c76b5089
gavinpennycuff <gavin@dereks-iMac.local>
2016-08-09 19:48:53 -0500

    useful messing around

---
+++ Chapter 2/variables.py
@@ -0,0 +1,38 @@
+Python 3.3.0 (v3.3.0:bd8afb90ebf2, Sep 29 2012, 01:25:11) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> 8 *3.57
+28.56
+>>> 365 * 10
+3650
+>>> +20
+20
+>>> day+year = 365
+SyntaxError: can't assign to operator
+>>> dayinyear = 365
+>>> print (dayinyear)
+365
+>>> print (norma)
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    print (norma)
+NameError: name 'norma' is not defined
+>>> day
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    day
+NameError: name 'day' is not defined
+
+>>> day_in_year = 365
+>>> 20 + 10 * 365 - 3 * 52
+3514
+>>> 
+>>> MLG = 420
+>>> 
+MLG = 360
+>>> Print (MLG)
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    Print (MLG)
+NameError: name 'Print' is not defined
+>>> 
