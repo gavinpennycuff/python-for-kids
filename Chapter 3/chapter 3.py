3287fac473251a7c3b8a8b96a54b5343667f8ddb
gavinpennycuff <gavin@dereks-iMac.local>
2016-08-09 19:49:57 -0500

    part of chapter 3

---
+++ Chapter 3/chapter 3.py
@@ -0,0 +1,47 @@
+Python 3.3.0 (v3.3.0:bd8afb90ebf2, Sep 29 2012, 01:25:11) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> MLG = "360 no scope"
+>>> print (MLG)
+360 no scope
+>>> move left = 'you are dead
+SyntaxError: invalid syntax
+>>> move_left = "game over"
+>>> normascore = 370
+>>> message = 'You scored
+SyntaxError: EOL while scanning string literal
+>>> 
+
+>>> 
+
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+5
+>>>  Normascore = normascore
+ 
+SyntaxError: unexpected indent
+>>> Normascore = normascore
+>>> message = 'I scored %s points'
+>>> Message = message
+>>> print (message % normascore)
+I scored 370 points
+>>> joke = '%s: a tool for finding mettal on ferniture in the dark'
+>>> Answer1 = 'knee'
+>>> Amswer2 = 'shin'
+>>> Answer2 = 'shin'
+>>> answer1 = Answer1
+>>> answer2 = Answer2
+>>> print (joke % answe2)
+Traceback (most recent call last):
+  File "<pyshell#27>", line 1, in <module>
+    print (joke % answe2)
+NameError: name 'answe2' is not defined
+>>> print ( joke % answer2)
+shin: a tool for finding mettal on ferniture in the dark
+>>> 
